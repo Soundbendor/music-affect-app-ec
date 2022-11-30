c181f1f1894c22fee501cdb9cb729f77573c170d
muradmiOSU <muradmi@oregonstate.edu>
2022-11-29 22:19:46 -0800

    formatted code using built in dart formatter

--- flutter_app/lib/tapper.dart
+++ flutter_app/lib/tapper.dart
@@ -17,9 +17,11 @@ class _TapperState extends State<Tapper> {
     return Listener(
         onPointerDown: (PointerDownEvent event) {
           // Global screen position.
-          print("Global position x:${event.position.dx}, y:${event.position.dy}");
+          print(
+              "Global position x:${event.position.dx}, y:${event.position.dy}");
           // Position relative to where this widget starts.
-          print("Relative position: x:${event.localPosition.dx}, y:${event.localPosition.dy}");
+          print(
+              "Relative position: x:${event.localPosition.dx}, y:${event.localPosition.dy}");
 
           var x = ((event.localPosition.dx - 17) - 180) / 180;
           var y = ((event.localPosition.dy - 180) * -1) / 180;
@@ -32,45 +34,42 @@ class _TapperState extends State<Tapper> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Row(
-                mainAxisAlignment: MainAxisAlignment.center
-                ,children: [
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
               Container(
                 decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(topLeft: Radius.circular(circleRadius)),
-                    color: Colors.pink[100]
-                ),
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(circleRadius)),
+                    color: Colors.pink[100]),
                 width: gridSize,
                 height: gridSize,
               ),
               Container(
                 decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(topRight: Radius.circular(circleRadius)),
-                    color: Colors.orange[100]
-                ),
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(circleRadius)),
+                    color: Colors.orange[100]),
                 width: gridSize,
                 height: gridSize,
                 // color: Colors.green,
               )
             ]),
-            Row(mainAxisAlignment: MainAxisAlignment.center,children: [
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
               Container(
                 decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(circleRadius)),
-                    color: Colors.grey[300]
-                ),
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(circleRadius)),
+                    color: Colors.grey[300]),
                 width: gridSize,
                 height: gridSize,
                 // color: Colors.red,
               ),
               Container(
                   decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(bottomRight: Radius.circular(circleRadius)),
-                      color: Colors.blue[100]
-                  ),
+                      borderRadius: BorderRadius.only(
+                          bottomRight: Radius.circular(circleRadius)),
+                      color: Colors.blue[100]),
                   width: gridSize,
-                  height: gridSize
-              )
+                  height: gridSize)
             ])
           ],
         ));
