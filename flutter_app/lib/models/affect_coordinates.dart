85f0bb7f3249eae2f034ed8886e8477abefcaed2
muradmiOSU <muradmi@oregonstate.edu>
2023-02-12 10:05:01 -0800

    added some doc comments

--- flutter_app/lib/models/affect_coordinates.dart
+++ flutter_app/lib/models/affect_coordinates.dart
@@ -1,12 +1,16 @@
+/// Class that holds on to latest affect data coordinates and generates an array
+/// that we can store to send to server
 class AffectCoordinates {
   double intervalCount = 0;
   double x  = 0.0;
   double y = 0.0;
   AffectCoordinates();
+  /// Use to add latest affect data tap
   void updateCoordinates(double x, double y) {
     this.x = x;
     this.y = y;
   }
+  /// this generates an array and increments the interval count
   List<double> generateArray() {
     intervalCount += 1.0;
     return [intervalCount, x, y];
--- flutter_app/lib/models/current_recording.dart
+++ flutter_app/lib/models/current_recording.dart
@@ -1,5 +1,6 @@
 import 'package:spotify_sdk/models/track.dart';
 
+/// class that represents the current recording of Affect data and track
 class CurrentRecording {
   final List<List<double>> _affectDataArray = [];
   Track? currentRecordingTrack;
@@ -11,19 +12,22 @@ class CurrentRecording {
 
   List<List<double>> get affectDataArray => _affectDataArray;
 
+  /// adds [data] to the array to be stored and sent to server
   void addDataToArray(List<double> data) {
     _affectDataArray.add(data);
   }
+  /// reset the data in the array
   void resetData() => _affectDataArray.clear();
 
+  /// lets us know if the current track has ended
   void currentTrackEnded() {
     isReadyToSubmit = true;
   }
 
+  /// resets the state of the class to the initial state
   void resetCurrentRecording() {
     resetData();
     currentRecordingTrack = null;
     isReadyToSubmit = false;
   }
-
 }
