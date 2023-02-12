73c5c6d111450cb299857adc8babf4ced413c52b
muradmiOSU <muradmi@oregonstate.edu>
2023-02-12 10:09:38 -0800

    Added some more comments explaining logic

--- flutter_app/lib/widgets/tapper.dart
+++ flutter_app/lib/widgets/tapper.dart
@@ -37,15 +37,17 @@ class _TapperState extends State<Tapper> {
   void didUpdateWidget(oldWidget) {
     super.didUpdateWidget(oldWidget);
     if (mounted) {
+      // only start the algorithm to collect data if the timer is null and we should start interval
       if (timer == null && widget.shouldStartInterval) {
         collectDataOnInterval();
       }
-      // if (timer != null && !widget.shouldStartInterval) {
-      //   if(timer!.isActive) {
-      //     timer?.cancel();
-      //     timer = null;
-      //   }
-      // }
+      // otherwise, lets cancel it and set the instance variable to null
+      if (timer != null && !widget.shouldStartInterval) {
+        if(timer!.isActive) {
+          timer?.cancel();
+          timer = null;
+        }
+      }
     }
   }
 
@@ -56,6 +58,8 @@ class _TapperState extends State<Tapper> {
   }
 
   void collectDataOnInterval() {
+    // this creates a function that loops every 1 second and adds whatever the
+    // latest data tapped was to the array that will be sent to server
     Timer.periodic(const Duration(seconds: 1), (Timer t) {
       setState(() {
         timer = t;
@@ -78,6 +82,8 @@ class _TapperState extends State<Tapper> {
 
           var x = ((event.localPosition.dx - 17) - 180) / 180;
           var y = ((event.localPosition.dy - 180) * -1) / 180;
+          // here we update AffectCoordinates, that the interval alg will add to
+          // the CurrentRecording array, to eventually be sent to the server
           addCoordinatesOnTap(x, y);
         },
         child: Column(
